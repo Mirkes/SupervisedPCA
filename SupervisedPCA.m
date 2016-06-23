b0e8aa7a79c08c8afe7a2a742cb8fdf313539c49
Evgeny Mirkes <em322@leicester.ac.uk>
2016-06-23 11:23:49 +0100

    Add short description of PCA and main function
    Add short description of PCA and main function

--- SupervisedPCA.m
+++ SupervisedPCA.m
@@ -1,17 +1,4 @@
-# SupervisedPCA
-Classification supervised PCA in accordance with Yehuda Koren and Liran Carmel
-
-Yehuda Koren and Liran Carmel introduced supervised PCA for classification problem in their paper
-
-Koren, Y., & Carmel, L. (2004). Robust linear dimensionality reduction. 
-IEEE transactions on visualization and computer graphics, 10(4), 459-470.
-
-Moreover they sugests three versions of PCA: normalized, supervised and normalized and supervised 
-simulateneously. Proposed MatLab function SupervisedPCA implements all these models. Standard PCA 
-can be calculated by this function as well. Also function allow to apply user defined Laplacian
-matrix.
-
-Description of function
+function [ V, D ] = SupervisedPCA( data, labels, nComp, kind )
 %SupervisedPCA calculates supervised PCA with respect to [1].
 %   [ V, D ] = SupervisedPCA( data, labels, nComp, kind ) return n-by-nComp
 %               matrix V with PCs as columns and diagonal nComp-by-nComp
@@ -46,3 +33,50 @@ Description of function
 %1. Koren, Yehuda, and Liran Carmel. "Robust linear dimensionality
 %   reduction." Visualization and Computer Graphics, IEEE Transactions on
 %   10.4 (2004): 459-470.
+
+    %Get sizes of data
+    n = size(data, 1);
+    
+    %Calculate Laplacian matrix
+    if ischar(kind)
+        if strcmpi(kind,'norm')
+            L = prepareNorm(data);
+        elseif strcmpi(kind,'super')
+            L = prepareSuper(labels);
+        elseif strcmpi(kind,'supernorm')
+            L = prepareNorm(data) .* prepareSuper(labels);
+        elseif strcmpi(kind,'usual')
+            L = repmat(-1,n,n);
+        else
+            error('Incorrect value of fourth argument');
+        end
+        %Recalculate diagonal
+        L = L - diag(sum(L));        
+    else
+        %Must be a matrix
+        [a,b] = size(kind);
+        if ~(isnumeric(kind) && ismatrix(kind) && a==b && a==n)
+            error('Incorrect value of fourth argument');
+        end
+        L = kind;
+    end
+    
+    %Calculate transformed covariance matrix
+    L = data'*L*data;
+    %Sum of variances
+    S = sum(diag(L));
+    %Request calculations from eigs
+    [ V, D ] = eigs(L, nComp);
+    D = D./S;
+end
+
+function L = prepareSuper(labels)
+    L = bsxfun(@minus,labels,labels');
+    L(L~=0) = -1;
+end
+
+function L = prepareNorm(data)
+    %Calculate distances between points
+    x = sum(data.^2,2);
+    L = sqrt(bsxfun(@plus,x,x')-2*(data*data'));
+end
